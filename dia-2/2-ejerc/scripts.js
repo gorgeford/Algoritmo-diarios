1bcb3ea6f5550391677fd64c769e596e9d9f90d5
gorgeford <gorgenavarro97@gmail.com>
2022-07-12 11:56:23 -0400

    dia-2

---
+++ dia-2/2-ejerc/scripts.js
@@ -0,0 +1,11 @@
+// Escribir una función que tome una cadena (a) como argumento
+// Extrae la primera mitad de la cadena
+// Devuelve el resultado
+
+
+const string = (a) => {
+
+    return a.slice(0, a.length / 2);
+}
+
+console.log(string('devuelve'));
