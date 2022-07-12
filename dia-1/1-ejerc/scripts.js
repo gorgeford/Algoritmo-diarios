b862e03f65ca2c950258e9d0d57778aebe9f34be
gorgeford <gorgenavarro97@gmail.com>
2022-07-11 23:00:56 -0400

    dia-1

---
+++ dia-1/1-ejerc/scripts.js
@@ -0,0 +1,12 @@
+//Escribe una función que tome una string como argumento
+// Extrae los últimos 3 caracteres de la cadena
+// Devuelve el resultado
+
+
+const string = (str) => {
+
+    return str.slice(-3);
+
+}
+
+console.log(string('abcdefg'));
---
+++ dia-1/2-ejerc/scripts.js
@@ -0,0 +1,13 @@
+//Escribe una función que tome una string (a) como argumento
+// Eliminar los primeros 3 caracteres del string, Devuelve el resultado
+
+
+
+const string = (a) => {
+    
+    return a.slice(3);
+    
+    }
+    
+    console.log(string('abcdef'));
+       
