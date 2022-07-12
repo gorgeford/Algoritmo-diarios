1e0aea5140e477f2074d6f1186995bbfce455b47
gorgeford <gorgenavarro97@gmail.com>
2022-07-12 11:54:50 -0400

    dia-2

---
+++ dia-2/1-ejerc/scripts.js
@@ -0,0 +1,11 @@
+//Escribe una función que tome una cadena (a) como argumento
+// Obtener los primeros 3 caracteres de la cadena
+// Devuelve el resultado
+
+
+const string = (a) => {
+
+    return a.slice(0,3);
+}
+
+console.log(string('123456'));
