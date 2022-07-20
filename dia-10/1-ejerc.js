026a6c9ed9a414e9f67a63d92ef99cb90f40cfe4
gorgeford <gorgenavarro97@gmail.com>
2022-07-20 17:55:00 -0400

    dia-10

---
+++ dia-10/1-ejerc.js
@@ -0,0 +1,17 @@
+//02-Comparison operators, strict equality
+ 
+// Escribe una función que tome dos valores, digamos a y b, como argumentos
+// Devuelve verdadero si los dos valores son iguales y del mismo tipo
+
+const values = (a, b) => {
+    
+    if (a === b){
+        return true;
+    }
+        return false;
+}
+
+console.log(values( 5, 5 ));
+
+
+
---
+++ dia-10/2-ejerc.js
@@ -0,0 +1,13 @@
+//03- Get type of value
+
+// Escribir una función que tome un valor como argumento
+// Devuelve el tipo del valor
+
+const values = (a) => {
+
+    return typeof(a);
+}
+
+console.log(values('hola'));
+
+
