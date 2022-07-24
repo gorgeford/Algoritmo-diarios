c33371bb1b4a87ea0b5986d7b988e38b6af1eb34
gorgeford <gorgenavarro97@gmail.com>
2022-07-24 12:38:10 -0400

    dia 11 agregados

---
+++ dia-11/3-ejerc.js
@@ -0,0 +1,12 @@
+//Javascript Array 
+
+//01 Get nth element of array
+
+// Escribir una función que tome una matriz (a) y un valor (n) como argumento
+// Devuelve el n-ésimo elemento de 'a'
+
+
+const value = (a, n) => a [n - 1];
+
+
+console.log(value([1,2,3,4,5],3));
---
+++ dia-11/4-ejerc.js
@@ -0,0 +1,21 @@
+
+// 02-Remove first n elements of an array 
+    
+// Escribir una función que tome una matriz (a) como argumento
+// Elimina los primeros 3 elementos de 'a'
+// Devuelve el resultado
+
+
+const Array = (a) => a.slice(3);
+
+console.log(Array([5,4,3,2,1,0]));
+
+
+
+
+
+
+
+
+
+
