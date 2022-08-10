c10fd53cc93a3072d30af580a315ba05e8277425
gorgeford <gorgenavarro97@gmail.com>
2022-08-10 10:03:59 -0400

    2-ejerc

---
+++ ejercicios-metodo-filter/2-ejerc.js
@@ -0,0 +1,12 @@
+//hacer una funcion que reciba un array de numeros y retorne un array con todos los numeros pares 
+
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const oddNumbers = () => {
+    
+      return numbers.filter(number => number % 2 === 0);
+      
+}
+
+console.log(oddNumbers(numbers));
